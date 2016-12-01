7ab791c1695438a42d1344fa876704b0cc58933e
hhovde <hhovde@machans.guest.local>
2016-12-01 22:32:14 +0100

    Database bootstrapper

--- src/main/java/no/norduni/oblig2/MyDB.java
+++ src/main/java/no/norduni/oblig2/MyDB.java
@@ -79,7 +79,7 @@ public class MyDB {
         try {
             this.execute(
                 "CREATE TABLE Reisende "
-                        + "(ID INT PRIMARY KEY, "
+                        + "(ID INT GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "
                         + "Name VARCHAR(255), "
                         + "Age INT, "
                         + "Passport VARCHAR(12), "
@@ -88,16 +88,66 @@ public class MyDB {
                         + "Gruppe VARCHAR(255), "
                         + "ShavenBalls BOOLEAN)"
             );
+            System.out.println("BootStrapped: Reisende!");
         } catch( SQLException e ) {
-         //   if( DerbyHelper.tableAlreadyExists( e ) ) {
-                return; // That's OK
-         //   }
-         //   throw e;
+            System.out.println("NOTE: Reisende finnes fra før..");
         }  
         
-        if(!this.tableExists("Flights")) {
-            //
+       try {
+            this.execute(
+                "CREATE TABLE Flights "
+                        + "(ID INT GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "
+                        + "FlightNr VARCHAR(255), "
+                        + "Origin VARCHAR(255), "
+                        + "Destination VARCHAR(12), "
+                        + "DepartureTime TIMESTAMP, "
+                        + "FlightDuration INT, "
+                        + "Seats INT)"
+            );
+            System.out.println("BootStrapped: Flights!");
+        } catch( SQLException e ) {
+            System.out.println("NOTE: Flights finnes fra før..");
         }
+
+        try {
+            this.execute(
+                "CREATE TABLE PassengerOnFlight "
+                        + "(ID INT GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "
+                        + "FlightID INT, "
+                        + "ReisendeID INT)"
+            );
+            System.out.println("BootStrapped: PassengerOnFlight!");
+        } catch( SQLException e ) {
+            System.out.println("NOTE: PassengerOnFlight finnes fra før..");
+            return; // That's OK
+        }  
+
+        try {
+            this.execute(
+                "CREATE TABLE PassengerInGroup "
+                        + "(ID INT GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "
+                        + "ReisendeID INT, "
+                        + "GruppeID INT)"
+            );
+            System.out.println("BootStrapped: PassengerInGroup!");
+        } catch( SQLException e ) {
+            System.out.println("NOTE: PassengerInGroup finnes fra før..");
+            return; // That's OK
+        }  
+
+        try {
+            this.execute(
+                "CREATE TABLE GroupOnFlight "
+                        + "(ID INT GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "
+                        + "FlightID INT, "
+                        + "GruppeID INT)"
+            );
+            System.out.println("BootStrapped: GroupOnFlight!");
+        } catch( SQLException e ) {
+            System.out.println("NOTE: GroupOnFlight finnes fra før..");
+       }  
+ 
+        /*
         if(!this.tableExists("PassengerOnFlight")) {
             //
         }
@@ -107,28 +157,9 @@ public class MyDB {
         if(!this.tableExists("GroupOnFlight")) {
             //
         }
-    
-        /*
-        try{
-            this.execute("ost");
-        } catch( SQLException e ) {
-            if( DerbyHelper.tableAlreadyExists( e ) ) {
-                return; // That's OK
-            }
-            throw e;
-        }        
-        */
-    }
- 
-/*    
-    public boolean bootStrapDB() {
-        if(!this.isBootStrapped()) {
-            this.tableExists(driver)
-        }
-        return true;
-    }
-*/
-            
+    */
+   }
+             
     public static void dbg(String info) {
         System.out.println(info);
     }
